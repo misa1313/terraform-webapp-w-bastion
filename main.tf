97dcd39a190e9d941cccf4767c4c02cb408af3ff
Misael <misael@misael.beat.com>
2023-08-22 14:14:07 -0400

    Moving from launch_configuration to launch_template.

--- main.tf
+++ main.tf
@@ -10,28 +10,43 @@ variable "access" {
 # Instances for the primary VPC - Autoscaling by CPU utilization
 ##########################################################################
 
-resource "aws_launch_configuration" "launch_config" {
-  name_prefix          = "apache-public-"
-  image_id             = "ami-08333bccc35d71140"
-  instance_type        = "t2.micro"
-  key_name             = "k-priv"
-  security_groups      = [aws_security_group.security-group-nat.id]
-  iam_instance_profile = aws_iam_instance_profile.apache-main-profile.name
-  user_data            = "${file("setup.sh")}"
+resource "aws_launch_template" "launch_template" {
+  name_prefix               = "apache-public-"
+  image_id                  = "ami-08333bccc35d71140"
+  instance_type             = "t2.micro"
+  key_name                  = "k-priv"
+  vpc_security_group_ids    = [aws_security_group.security-group-nat.id]
+  user_data                 = "${file("setup.sh")}"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.apache-main-profile.name
+  }
+  metadata_options {
+    instance_metadata_tags      = "enabled"
+  }
+  monitoring {
+    enabled = true
+  }
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      environment = "dev"
+    }
+  }
 }
 
 resource "aws_autoscaling_group" "autoscaling_group" {
-  launch_configuration = aws_launch_configuration.launch_config.name
-  min_size             = 2
-  max_size             = 4
-  desired_capacity     = 2
-  vpc_zone_identifier  = [aws_subnet.private-subnet.id]
-  target_group_arns    = [aws_lb_target_group.target-group.arn]
+  launch_template           = aws_launch_template.launch_template.name
+  min_size                  = 2
+  max_size                  = 4
+  health_check_grace_period = 300
+  vpc_zone_identifier       = [aws_subnet.private-subnet.id]
+  target_group_arns         = [aws_lb_target_group.target-group.arn]
 }
 
 resource "aws_autoscaling_policy" "scale-policy" {
   name                   = "scale-policy"
   adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = 1
   policy_type            = "TargetTrackingScaling"
   autoscaling_group_name = aws_autoscaling_group.autoscaling_group.name
 
